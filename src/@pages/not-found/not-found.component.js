eef11d2b7b11c9ebe97961649b49d0b7885f2325
kgyuzalyan <kgyuzalyan@gmail.com>
2020-09-05 19:22:53 +0300

    [Add]: Add pages - home, not-found

---
+++ src/@pages/not-found/not-found.component.js
@@ -0,0 +1,11 @@
+import React, { PureComponent } from 'react';
+
+export default class NotFound extends PureComponent {
+  render() {
+    return (
+      <div className="not-found">
+        <h1 className="not-found-heading">NotFound Page!</h1>
+      </div>
+    );
+  }
+}
